5d434c87959d26bc5c26d43d718ddc3ef7cd272f
Shika <zcmate@gmail.com>
2019-02-14 12:10:37 +0100

    refactor front-end added css+react

---
+++ view/footer.php
@@ -0,0 +1,6 @@
+    <script src="js/babel.min.js"></script>
+    <script src="js/materialize.min.js"></script>
+    <script src="js/react.production.min.js"></script>
+    <script src="js/react-dom.production.min.js"></script>
+</body>
+</html>
