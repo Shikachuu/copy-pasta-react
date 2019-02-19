2e650a1f9983d84ab0d9976795a8c1681126d9a4
Shika <zcmate@gmail.com>
2019-02-19 08:15:16 +0100

    fixin the button layout

--- view/regilogin.php
+++ view/regilogin.php
@@ -1,41 +1,43 @@
 <?php include('header.php'); ?>
 <div class="container">
     <div class="col l5 s12">
-        <h1 class="center-align white-text">Login</h1>
-        <form action="../model/mlogin.php" method="post" class="row">
-            <div class="input-field col l5 s6">
-                <input id="luser" type="text" class="validate">
-                <label for="luser">Username</label>
-            </div>
-            <div class="input-field col l5 s6">
-                <input id="lpass" type="password" class="validate">
-                <label for="lpass">Password</label>
+        <h2 class="center-align white-text">Login</h2>
+        <form action="../model/mlogin.php" method="post" >
+            <div class="row">
+                <div class="input-field col l5 s6">
+                    <input id="luser" type="text" class="validate">
+                    <label for="luser">Username</label>
+                </div>
+                <div class="input-field col l5 s6">
+                    <input id="lpass" type="password" class="validate">
+                    <label for="lpass">Password</label>
+                </div>
             </div>
             <button class="btn waves-effect waves-light" type="submit" name="action">Login<i class="material-icons right">how_to_reg</i></button>
         </form>
-        <h1 class="center-align white-text">Register</h1>
-        <form action="mregi.php" method="post" class="row">
-            <div class="input-field col l5 s6">
-                <input type="text" name="ruser" id="ruser" class="validate">
-                <label for="ruser">Username</label>
-            </div>
-            <div class="input-field col l5 s6">
-                <input type="email" name="remail" id="remail" class="validate">
-                <label for="remail">Email</label>
-            </div>
-            <div class="input-field col l5 s6">
-                <input type="password" name="rpass" id="rpass" class="validate">
-                <label for="rpass">Password</label>
-            </div>
-            <div class="input-field col l5 s6">
-                <input type="password" name="rpassag" id="rpassag" class="validate">
-                <label for="rpassag">Verify Password</label>
+        <h2 class="center-align white-text">Register</h2>
+        <form action="mregi.php" method="post">
+            <div class="row">
+                <div class="input-field col l5 s6">
+                    <input type="text" name="ruser" id="ruser" class="validate">
+                    <label for="ruser">Username</label>
+                </div>
+                <div class="input-field col l5 s6">
+                    <input type="email" name="remail" id="remail" class="validate">
+                    <label for="remail">Email</label>
+                </div>
+                <div class="input-field col l5 s6">
+                    <input type="password" name="rpass" id="rpass" class="validate">
+                    <label for="rpass">Password</label>
+                </div>
+                <div class="input-field col l5 s6">
+                    <input type="password" name="rpassag" id="rpassag" class="validate">
+                    <label for="rpassag">Verify Password</label>
+                </div>
             </div>
             <button class="btn waves-effect waves-light" type="submit" name="action">Register<i class="material-icons right">person_add</i></button>
         </form>
     </div>
-    <div class="col l5 offset-l5 s12">
-    
-    </div>
+    <br>
 </div>
 <?php include('footer.php'); ?>
