e0f9ae8a0412996d14c8db0af43457d493009470
Shika <zcmate@gmail.com>
2019-03-06 00:54:58 +0100

    finished userservice (added login+ logout + regi autologin)

--- model/userservice.php
+++ model/userservice.php
@@ -11,19 +11,14 @@
             $mysql = db::get();
             $this->mysqldb = $mysql;
         }
-        private function validate($username,$password,$email)
+        private function validate($username,$password)
         {
             $valid = false;
-            if (!empty($username) && !empty($password) && !empty($email)) {
+            if (!empty($username) && !empty($password)) {
                 if (strlen($password) >= 8) {
-                    if (strpos($email,"@") && strpos($email,".")) {
-                        $this->username = $this->mysqldb->escape($username);
-                        $this->password = md5($this->mysqldb->escape($password));
-                        $this->email = $this->mysqldb->escape($email);
-                        $valid = true;
-                    }else {
-                        $this->errorMSG += " The email address isn't valid.";
-                    }
+                    $this->username = $this->mysqldb->escape($username);
+                    $this->password = md5($this->mysqldb->escape($password));
+                    $valid = true;
                 }else {
                     $this->errorMSG += " The password must be atleast 8 char long.";
                 }
@@ -31,6 +26,17 @@
                 $this->errorMSG += " You should fill all the fields.";
             }
             return $valid;
+        }
+        // NOTE: separated cause of the login
+        private function validateMail($email)
+        {
+            if (strpos($email,"@") && strpos($email,".")) {
+                $this->email = $this->mysqldb->escape($email);
+                return true;
+            }else {
+                $this->errorMSG += " The email address isn't valid.";
+                return false;
+            }
         }
         // NOTE: always run validaate before isUserExist!!
         private function isUserExist()
@@ -46,12 +52,13 @@
         public function register($rusername,$rpassword,$rpasswordag,$remail)
         {
             $success = false;
-            if ($this->validate($rusername,$rpassword,$remail)) {
+            if ($this->validate($rusername,$rpassword) && $this->validateMail($remail)) {
                 if ($this->isUserExist() === false) {
                     if ($this->password == md5($rpasswordag)) {
                         $this->mysqldb->query("INSERT INTO users (`username`, `password`,`email`) VALUES ($this->username,$this->password,$this->email)");
-                        $this->login($this->username,$this->password);
-                        return "The registration was successful.";
+                        $_SESSION["user_id"] = $this->mysqldb->insert_id();
+                        $_SESSION["username"] = $this->username;
+                        return "The registration was successful. You have been automaticaly logged in.";
                     }
                 }else {
                     return $this->errorMSG;
@@ -60,9 +67,37 @@
                 return $this->errorMSG;
             }
         }
-        public function login($username,$password)
+        public function login($lusername,$lpassword)
         {
-            
+            if ($this->validate($lusername,$lpassword)) {
+                if ($this->isUserExist() === true) {
+                    $dummy = $this->mysqldb->query("SELECT * FROM users WHERE username=".$this->username);
+                    if (password_verify($lpassword,$dummy["password"])) {
+                        //session_start();
+                        $_SESSION["user_id"] = $dummy["user_id"];
+                        $_SESSION["username"] = $this->username;
+                        return "Login successfull, Welcome ".$this->username;
+                    }else {
+                        $this->errorMSG = "Wrong password.";
+                        return $this->errorMSG;
+                    }
+                }else {
+                    return $this->errorMSG;
+                }
+            }else {
+                return $this->errorMSG;
+            }
+        }
+        public function logout()
+        {
+            if (isset($_SESSION["user_id"])&&isset($_SESSION["username"])) {
+                session_unset();
+                session_destroy();
+                return "You log out successfully.";
+            }else {
+                $this->errorMSG = "You have to log in first.";
+                return $this->errorMSG;
+            }
         }
     }
 ?>
