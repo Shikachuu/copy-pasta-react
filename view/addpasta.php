8798151b276cf783282881a606453ca0affb1e3c
Czékus Máté <zcmate@gmail.com>
2019-03-20 18:20:02 +0100

    trying to get hljs languages to options

--- view/addpasta.php
+++ view/addpasta.php
@@ -2,7 +2,7 @@
 <div id="modal1" class="modal">
     <div class="modal-content">
         <h4 class="center-align text-white">Add pasta</h4>
-        <form action="uaddpastac.php" method="post">
+        <form action="../controller/uaddpastac.php" method="post">
             <input type="text" name="pasta_name" id="pasta_name">
             <input type="textarea" name="pasta_content" id="pasta_content">
             <select name="language" id="language">
@@ -22,7 +22,7 @@
 <div id="modal1" class="modal blue darken-3">
     <div class="modal-content">
         <h4 class="center-align white-text">Add pasta</h4>
-        <form action="gaddpastac.php" method="post">
+        <form action="../controller/gaddpastac.php" method="post">
             <input type="text" name="pasta_name" id="pasta_name">
             <textarea class="materialize-textarea" name="pasta_content" id="pasta_content" cols="30" rows="10"></textarea>
             <input type="password" name="password" id="password">
@@ -42,6 +42,9 @@
 <?php }?>
 <script src="js/jquery-3.3.1.min.js"></script>
 <script>
+const languages = listLanguages();
+console.log(languages);
+}
  $(document).ready(function(){
     $('.modal').modal();
  });
