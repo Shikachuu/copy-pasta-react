d6e9387782d77e3e2105eebb53daf2370bfef9e4
Shika <zcmate@gmail.com>
2019-03-30 13:59:04 +0100

    fixed php routing?

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-header("Location: ./view/index.php");
+    $request_uri = explode('?', $_SERVER['REQUEST_URI'],2);
+    switch ($request_uri[0]) {
+        case '/':
+            require'../view/index.php';
+            break;
+        default:
+            header('HTTP/1.0 404 Not Found');
+            require'../view/404.php';
+            break;
+    }
 ?>
