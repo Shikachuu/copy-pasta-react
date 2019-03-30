04ace74c418cac29edba6e8f1ec73464142923a3
Shika <zcmate@gmail.com>
2019-03-30 18:21:26 +0100

    remvoed php routing cause its working state implemented CSICSKAMEGOLDÁS instead

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
 <?php
-    $request_uri = explode('?', $_SERVER['REQUEST_URI'],2);
-    switch ($request_uri[0]) {
-        case '/':
-            require'../view/index.php';
-            break;
-        default:
-            header('HTTP/1.0 404 Not Found');
-            require'../view/404.php';
-            break;
-    }
+    header('Location:/view/index.php');
 ?>
